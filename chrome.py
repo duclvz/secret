ae0b08162935f5b44dbdf81d39eedc0e77e57f51
Duc Lv <duclvzz@gmail.com>
2016-03-01 08:58:27 +0000

    fix chrome.py

--- chrome.py
+++ chrome.py
@@ -5,11 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
+import random
 display = Display(visible=0, size=(1280, 720))
 display.start()
 
 options = Options()
-
 options.add_argument('--user-data-dir=/root/chromeBotTE')
 # options.add_argument('--user-agent="Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"')
 mobile_emulation = {
@@ -18,27 +18,54 @@ mobile_emulation = {
 options.add_experimental_option("mobileEmulation", mobile_emulation)
 youtube = webdriver.Chrome(chrome_options = options)
 youtube.get('https://www.youtube.com/watch?v=uYRuxz_UIOI&list=PLNFHZ147pUItjKgODlwExeXrfRkHsk4uH')
-time.sleep(3)
-youtube.execute_script('if (document.getElementsByClassName("_mrbc _mjib").length>0) { document.getElementsByClassName("_mrbc _mjib")[0].click() } else if(document.getElementsByClassName("_mno").length>0) { document.getElementsByClassName("_mno")[0].click() }')
-print youtube.title
-# video = driver.find_element_by_class_name("_mrbc")
-# video.click()
-# driver.save_screenshot('screenie.png')
-try:
-    if youtube.find_element_by_tag_name('video').get_attribute('paused')=='true':
-        youtube.find_element_by_css_selector('._mrbc._mjib').click()
-        youtube.find_element_by_css_selector('._mno').click()
-    else:
-        print "Da chay Video"
-except:
-    print "Da chay Video"
-Số video playlist
-._mzr 1 / 4
-playlist-length 329 videos
 
+def playVideo():
+    try:
+        youtube.find_element_by_css_selector('div[aria-label="Play"]').click()
+        print('Đã chạy Video - div[aria-label="Play"]')
+    except:
+        try:
+            youtube.find_element_by_css_selector('a[title="Play video"]').click()
+            print('Đã chạy Video - a[title="Play video"]')
+        except:
+            print("Lỗi khi click Video")
+
+def nextVideo():
+    try:
+        youtube.find_element_by_css_selector('.ytp-next-button.ytp-button').click()
+        print('Đã Next Video')
+    except:
+        try:
+            youtube.find_element_by_css_selector('a[title="Next video"]').click()
+            print('Đã Next Video')
+        except:
+            print('Lỗi khi Click Next Video')
+def percent():
+    if random.uniform(0,100)>20:
+        return random.uniform(65,85)/100
+    else:
+        return random.uniform(20,100)/100
+# Lấy độ dài playlist
 try:
-    youtube.find_element_by_css_selector('.ytp-next-button.ytp-button').click()
-    youtube.find_element_by_css_selector('._maw._mch').click()
+    listlen = int(youtube.find_element_by_css_selector('div._mpr').text.split(' / ')[1])
 except:
-    print "Da next Video"
+    try:
+        listlen = int(youtube.find_element_by_css_selector('span#playlist-length').text.split(' ')[0])
+    except:
+        print('Lỗi khi lấy độ dài playlist')
+# Loop
+for x in xrange(1, listlen):
+    print('Đang xem video '+youtube.title)
+    playVideo()
+    time.sleep(3)
+    try:
+        rdper=percent();
+        print('Chờ xem video với tỉ lệ thời gian '+str(rdper))
+        time.sleep(float(youtube.find_element_by_tag_name('video').get_attribute('duration'))*rdper)
+        youtube.save_screenshot('screenie.png')
+        nextVideo()
+    except:
+        print('Lỗi khi chờ xem video')
+
+
         
