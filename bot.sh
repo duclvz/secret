747809332c2e12569eea62de843dcfd7e06d9d7b
Duc Lv <duclvzz@gmail.com>
2016-03-23 10:58:49 +0000

    fix lsb

--- bot.sh
+++ bot.sh
@@ -33,7 +33,7 @@ apt-get update
 apt-get clean
 apt-get autoclean
 apt-get autoremove -y
-apt-get install -y psmisc
+apt-get install -y psmisc lsb-release
 apt-get install -y xvfb x11-xkb-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable xfonts-cyrillic x11-apps
 apt-get install -y gtk2-engines-pixbuf libexif12 libxpm4 libxrender1 libgtk2.0-0
 apt-get install -y libnss3 libgconf-2-4
