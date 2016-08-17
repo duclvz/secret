cb6ef2377fe70fed8558009ea856a2af5bcf6dd8
Duc Lv <duclvzz@gmail.com>
2016-08-17 12:57:54 +0000

    Update

--- getRT.js
+++ getRT.js
@@ -1,37 +1,34 @@
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
-
+var fs = require('fs');
+var credential = require('./credential.json');
 var readline = require('readline');
 var rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-var credential = {
-	client_id: '197517672022-vm8g45cqp92oa8tfue1cklkgqfjg05rt.apps.googleusercontent.com',
-	client_secret: 'cmYoue69Ao6jvSodhgWQPMBs',
-	redirect_uri: 'urn:ietf:wg:oauth:2.0:oob'
-};
-var oauth2Client = null;
 var scopes = [
 	'https://www.googleapis.com/auth/youtube',
 	'https://www.googleapis.com/auth/youtube.upload'
 ];
 
-oauth2Client = new OAuth2(credential.client_id, credential.client_secret, credential.redirect_uri);
+var oauth2Client = new OAuth2(credential.installed.client_id, credential.installed.client_secret, credential.installed.redirect_uris[0]);
 console.log(oauth2Client.generateAuthUrl({
 	access_type: 'offline',
 	scope: scopes
 }))
-getRT();
 
-function getRT() {
-	rl.question('Code: ', function(code) {
-		oauth2Client.getToken(code, function(err, tokens) {
-			if (!err) {
-				console.log(tokens)
-			}
-		});
-		rl.close();
-	})
-}
+rl.question('Code: ', function(code) {
+	oauth2Client.getToken(code, function(err, tokens) {
+		if (!err) {
+			console.log(tokens)
+			credential.installed.refresh_token = tokens.refresh_token;
+			fs.writeFile('./credential.json', JSON.stringify(credential), 'utf8', function() {
+				console.log('Write Credential to');
+				console.log(credential);
+			})
+		}
+	});
+	rl.close();
+})
--- upload.js
+++ upload.js
@@ -1,33 +1,24 @@
 var fs = require('fs');
+var request = require('request');
 var youtubedl = require('youtube-dl');
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
-var oauth2Client = new OAuth2('197517672022-vm8g45cqp92oa8tfue1cklkgqfjg05rt.apps.googleusercontent.com', 'cmYoue69Ao6jvSodhgWQPMBs', 'urn:ietf:wg:oauth:2.0:oob');
-
-var scopes = [
-	'https://www.googleapis.com/auth/youtube',
-	'https://www.googleapis.com/auth/youtube.upload'
-];
-
-var url = oauth2Client.generateAuthUrl({
-	access_type: 'offline',
-	scope: scopes
-});
+var credential = require('./credential.json');
+var oauth2Client = new OAuth2(credential.installed.client_id, credential.installed.client_secret, credential.installed.redirect_uris[0]);
 
 oauth2Client.setCredentials({
-	refresh_token: '1/rUPYOsfmvxmcKokY18sO6LQMrIEEFKT9daVU1mdjUDM'
+	refresh_token: credential.installed.refresh_token
 });
 
 var Youtube = require('youtube-api');
-var lastTime = require("./lastTime.json")
 
-function refresh(callback) {
+function refresh() {
 	oauth2Client.refreshAccessToken(function(err, tokens) {
 		Youtube.authenticate({
 			type: "oauth",
 			refresh_token: tokens.refresh_token,
-			client_id: "197517672022-vm8g45cqp92oa8tfue1cklkgqfjg05rt.apps.googleusercontent.com",
-			client_secret: "cmYoue69Ao6jvSodhgWQPMBs",
+			client_id: credential.installed.client_id,
+			client_secret: credential.installed.client_secret,
 			token: tokens.access_token
 		});
 	});
@@ -37,57 +28,80 @@ refresh();
 setInterval(function() {
 	refresh();
 }, 3500000)
-// var handler = setInterval(function() {
-// 	Youtube.search.list({
-// 		part: 'id,snippet',
-// 		channelId: 'UCrMxXQAHxzQEWVhVhR9oXeQ',
-// 		order: 'date',
-// 		maxResults: 40,
-// 		publishedAfter: lastTime.time
-// 	}, function(err, data) {
-// 		if (!err) {
-// 			console.log(data);
-// 			console.log(new Date().toString());
-// 			for (var i = data.items.length - 1; i >= 0; i--) {
-// 				var tempTime = new Date(data.items[i].snippet.publishedAt);
-// 				tempTime.setTime(tempTime.getTime() + 1000);
-// 				lastTime.time = tempTime.toISOString();
-// 				fs.writeFile('./lastTime.json', JSON.stringify(lastTime), 'utf8', function() {
-// 					console.log('Write lastTime to');
-// 					console.log(lastTime);
-// 				})
-// 				var videoId = data.items[i].id.videoId;
-// 				if (videoId)
-// 					reup(videoId);
-// 			}
-// 		}
-// 	})
-// }, 180000);
+
+var handler = setInterval(function() {
+	Youtube.search.list({
+		part: 'id,snippet',
+		channelId: 'UC7zhwOWu8rRl9VEg50uSXNA',
+		order: 'date',
+		maxResults: 50,
+		publishedAfter: credential.installed.lastTime
+	}, function(err, data) {
+		if (!err) {
+			console.log(data)
+			console.log(new Date().toString());
+			for (var i = data.items.length - 1; i >= 0; i--) {
+				var tempTime = new Date(data.items[i].snippet.publishedAt);
+				tempTime.setTime(tempTime.getTime() + 1000);
+				credential.installed.lastTime = tempTime.toISOString();
+				fs.writeFile('./credential.json', JSON.stringify(credential), 'utf8', function() {
+					console.log('[Change lastTime] Write Credential to');
+					console.log(credential);
+				})
+				var videoId = data.items[i].id.videoId;
+				if (videoId)
+					reup(videoId);
+			}
+		}
+	})
+}, 300000);
 
 function reup(videoId) {
-	setTimeout(function() {
-		youtubedl.getInfo('http://www.youtube.com/watch?v=' + videoId, ['-f best'], function(err, info) {
-			if (err) throw err;
-			var video = youtubedl('http://www.youtube.com/watch?v=' + videoId, ['-f best']);
-			Youtube.videos.insert({
-				resource: {
-					snippet: {
-						title: info.title,
-						description: info.description,
-						tags: info.tags,
-						categoryId: '17'
-					},
-					status: {
-						privacyStatus: "public"
-					}
-				},
-				part: "snippet,status",
-				media: {
-					body: video
-				}
-			}, function(err, data) {
-				console.log("Uploaded Video " + info.title);
-			});
-		})
-	}, 180000)
+	youtubedl.getInfo('http://www.youtube.com/watch?v=' + videoId, ['-f best'], function(err, info) {
+		if (err) throw err;
+		upload(videoId, info);
+	})
+}
+
+function upload(videoId, info) {
+	var video = youtubedl('http://www.youtube.com/watch?v=' + videoId, ['-f best']);
+	Youtube.videos.insert({
+		resource: {
+			snippet: {
+				title: info.title,
+				description: info.description,
+				tags: info.tags,
+				categoryId: '25'
+			},
+			status: {
+				privacyStatus: "public"
+			}
+		},
+		part: "snippet,status",
+		media: {
+			body: video
+		}
+	}, function(err, data) {
+		if (err) return console.log("Upload failed " + info.title);
+		console.log("Uploaded Video " + info.title);
+		if (data)
+			setThumb(info, data.id);
+	});
+}
+
+function setThumb(info, newVideoId) {
+	request({
+		url: info.thumbnail,
+		encoding: null
+	}, function(err, res, body) {
+		Youtube.thumbnails.set({
+			videoId: newVideoId,
+			media: {
+				body: body
+			}
+		}, function(err, data) {
+			if (!err)
+				console.log("Setted up thumbnail of " + info.title)
+		});
+	});
 }
